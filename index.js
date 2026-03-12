af93029de7a5185a905ff0fed2deefd868cec81e
fahim6855 <fahimislam6855@gmail.com>
2026-03-12 09:10:35 +0000

    db token env

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ import { createClient } from "@libsql/client";
 import bcrypt from "bcrypt";
 import { jwt, sign } from "hono/jwt";
 
-let dbToken =
-  "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzMxMTE1MDQsImlkIjoiMDE5Y2Q1YWUtMTIwMS03MTQ1LWE1MGEtOWRlNmQ0NTA4MjI4IiwicmlkIjoiMzE0YzNhNWYtMTRjNy00MGRhLWI0MzYtMWNjYzZjMmU3YmVmIn0.IhOQwb932E56GSFb3njizQDRebbbJamJyS-i1_axsVp5tlTIdv-4rgU5w0Jh_2HiXyrgbnGR-gG_jv9kzaV1CQ";
+let dbToken = process.env.TURSO_DB_TOKEN;
 
 //Middlewares
 const app = new Hono();
